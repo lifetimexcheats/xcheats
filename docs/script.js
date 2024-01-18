6613f77d21cdf4355e5e9be52898148a07493076
McDumfly <69152889+McDumfly@users.noreply.github.com>
2024-01-18 01:05:24 +0100

    Add files via upload

---
+++ docs/script.js
@@ -0,0 +1,46 @@
+let dtLetters = [[
+    "N", "e", "x", "t", "G", "e", "n" 
+], [
+    "C", "h", "e", "a", "p"
+], [
+    "C", "u", "s", "t", "o", "m", "i", "z", "a", "b", "l", "e"
+], [
+    "L", "i", "f", "e", "t", "i", "m", "e"
+], [
+    ""
+]];
+
+function gebi(id) {
+    return document.getElementById(id);
+}
+
+let currLetter = 0;
+let letterSet = 0;
+function changeTitle() {
+    setTimeout(() => {
+        if (letterSet > 3) {
+            letterSet = 0;
+            currLetter = 0;
+        }
+        if (currLetter > dtLetters[letterSet].length - 1 || document.title == "XCheats | ") {
+            currLetter = 0;
+            document.title = "XCheats | ";
+            letterSet++;
+        }
+        document.title += " " + dtLetters[letterSet][currLetter];
+        currLetter++;
+        changeTitle();
+    }, 500);
+}
+
+changeTitle();
+
+buyBtnState = false;
+function checkBuyBtnState() {
+    if (buyBtnState) {
+        gebi("buy-btn").classList.add("hidden");
+    } else {
+        gebi("buy-btn").classList.remove("hidden");
+    }
+    buyBtnState = !buyBtnState;
+}
